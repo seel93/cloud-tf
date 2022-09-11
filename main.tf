8bdd15a7c619e8afafbf6bec108adebfa3462cd7
Øystein Seel <A2110211@nbsemp.no>
2022-09-11 18:21:09 +0200

    remove invalid module var

--- main.tf
+++ main.tf
@@ -14,8 +14,6 @@ module "vnet" {
 
 module "vm" {
   source = "./modules/vm"
-  network_interface_1_id = module.vnet.network_interface_1_id
-  network_interface_2_id = module.vnet.network_interface_2_id
   resource_group_location = var.resource_group_location
   resource_group_name_prefix = var.resource_group_name
-}
+}K
