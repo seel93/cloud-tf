a83d1b051106396f54adcf6db813934f547d147f
Øystein Seel <A2110211@nbsemp.no>
2022-09-13 19:17:36 +0200

    break up resources into seperate files & add lb

---
+++ network.tf
@@ -0,0 +1,77 @@
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    address_prefix = "10.0.0.0/24"
+    name           = "subnet1"
+  }
+
+  subnet {
+    address_prefix = "10.0.1.0/24"
+    name           = "subnet2"
+  }
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_virtual_network.my_terraform_network.subnet.*.id[0]
+    private_ip_address_allocation = "Dynamic"
+    //public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+resource "azurerm_network_interface" "my_second_terraform_nic" {
+  name                = "mySecondNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_virtual_network.my_terraform_network.subnet.*.id[1]
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
